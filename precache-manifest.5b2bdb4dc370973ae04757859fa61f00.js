54c37dba8c7ec3c0b60f0d482b64df6ae62ac9ee
Florisnl88 <40896560+Florisnl88@users.noreply.github.com>
2020-09-15 20:07:11 +0200

    RoboFriends commit

---
+++ precache-manifest.5b2bdb4dc370973ae04757859fa61f00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b284f2d09a4259227a96c94b58031d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da5f820473674468c619",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "40c6811f4bb3195c70c5",
+    "url": "/static/css/main.b10d03c3.chunk.css"
+  },
+  {
+    "revision": "da5f820473674468c619",
+    "url": "/static/js/2.f62c00ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f62c00ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c6811f4bb3195c70c5",
+    "url": "/static/js/main.17b98588.chunk.js"
+  },
+  {
+    "revision": "c1e11704814894dbd6c3",
+    "url": "/static/js/runtime-main.d470ab75.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
